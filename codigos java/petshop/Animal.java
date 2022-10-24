5023991140fc84816f19d3b95de9c7f0619bc1fb
SarahElesbao <79275710+SarahElesbao@users.noreply.github.com>
2022-10-24 18:31:32 -0300

    Update Animal.java

--- codigos java/petshop/Animal.java
+++ codigos java/petshop/Animal.java
@@ -10,4 +10,61 @@ public class Animal {
     float peso;
     Date dtnascimento;
     char sexo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public Date getDtnascimento() {
+        return dtnascimento;
+    }
+
+    public void setDtnascimento(Date dtnascimento) {
+        this.dtnascimento = dtnascimento;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
 }
