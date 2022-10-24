b5a6c5644ca599cdbc64e4239032bf5804389b3c
SarahElesbao <79275710+SarahElesbao@users.noreply.github.com>
2022-10-24 18:32:37 -0300

    Update Endereco.java

--- codigos java/petshop/Endereco.java
+++ codigos java/petshop/Endereco.java
@@ -7,5 +7,53 @@ public class Endereco {
     String bairro;
     String cidade;
     int cep;
-    
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
 }
