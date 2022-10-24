44d262fd8a620ab5e34d0de30e23c60beae6eaf8
SarahElesbao <79275710+SarahElesbao@users.noreply.github.com>
2022-10-24 18:31:50 -0300

    Update App.java

--- codigos java/petshop/App.java
+++ codigos java/petshop/App.java
@@ -4,11 +4,6 @@ public class App {
 
     public static void main(String[] args){
         
-        Pessoa pessoa = new Pessoa();
-        pessoa.nome = "Sarah Elesbão";
-        pessoa.telefone = 40028922;
-        pessoa.cpf = 1734827346;
-
         Endereco endereco = new Endereco();
         endereco.rua = "Rua da Sarah";
         endereco.numero = 443;
@@ -17,7 +12,26 @@ public class App {
         endereco.cidade = "Rio de Janeiro";
         endereco.cep = 21534289;
 
+        Animal animal = new Animal();
+        animal.nome = "mirabel";
+        animal.raca = "gato";
+        animal.porte = "pequeno";
+        animal.tipo = "siamês";
+        animal.peso = 4;
+        //dtnascimento = 01/02/2020;
+        animal.sexo = 'm';
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Sarah Elesbão";
+        pessoa.telefone = 40028922;
+        pessoa.cpf = "173.482.734-96";
         pessoa.moradia = endereco;
+        pessoa.bicho = animal;
+
+        pessoa.moradia = endereco; 
+
+        pessoa.info();
+
 
     }
     
