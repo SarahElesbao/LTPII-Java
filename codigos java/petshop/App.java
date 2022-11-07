c6b5856a274a9f289bd1c4d45318296c9049047f
Sarah Elesbão <79275710+SarahElesbao@users.noreply.github.com>
2022-11-07 20:52:12 -0300

    Update App.java

--- codigos java/petshop/App.java
+++ codigos java/petshop/App.java
@@ -2,37 +2,40 @@ package petshop;
 
 public class App {
 
-    public static void main(String[] args){
-        
-        Endereco endereco = new Endereco();
-        endereco.rua = "Rua da Sarah";
-        endereco.numero = 443;
-        endereco.complemento = "fundos";
-        endereco.bairro = "Pavuna";
-        endereco.cidade = "Rio de Janeiro";
-        endereco.cep = 21534289;
+    public static void main(String[] args) {
 
-        Animal animal = new Animal();
-        animal.nome = "mirabel";
-        animal.raca = "gato";
-        animal.porte = "pequeno";
-        animal.tipo = "siamês";
-        animal.peso = 4;
-        //dtnascimento = 01/02/2020;
-        animal.sexo = 'm';
+        Endereco endereco = new Endereco("rua são paulão", 325, "fundos", "pavuna", "rio de janeiro", 34561233);
+        // endereco.rua = "Rua da Sarah";
+        // endereco.numero = 443;
+        // endereco.complemento = "fundos";
+        // endereco.bairro = "Pavuna";
+        // endereco.cidade = "Rio de Janeiro";
+        // endereco.cep = 21534289;
 
-        Pessoa pessoa = new Pessoa();
-        pessoa.nome = "Sarah Elesbão";
-        pessoa.telefone = 40028922;
-        pessoa.cpf = "173.482.734-96";
-        pessoa.moradia = endereco;
-        pessoa.bicho = animal;
+        Animal animal = new Animal("mirabel", "gato", "pequeno", "siamês", 5, "16/02/2020", 'm');
+        // animal.nome = "mirabel";
+        // animal.raca = "gato";
+        // animal.porte = "pequeno";
+        // animal.tipo = "siamês";
+        // animal.peso = 4;
+        // dtnascimento = 01/02/2020;
+        // animal.sexo = 'm';
 
-        pessoa.moradia = endereco; 
+        //Pessoa pessoa = new Pessoa("Sarah Elesbão", 40028922, endereco, animal);
+        // pessoa.nome = "Sarah Elesbão";
+        // pessoa.telefone = 40028922;
+        // pessoa.cpf = "173.482.734-96";
+        // pessoa.moradia = endereco;
+        // pessoa.bicho = animal;
 
-        pessoa.info();
+        // pessoa.moradia = endereco;
+        Juridica juridica = new Juridica("59.927.884/0001-77", "Sarah Elesbão", 40028922, endereco, animal);
 
+        Fisica fisica = new Fisica("437.715.400-11", "Sarah Elesbão", 40028922, endereco, animal);
 
+        //pessoa.info();
+        fisica.info();
+        juridica.info();
     }
-    
+
 }
