ed7e27d67ead248c248ad8e70d85cbbe9becbc52
Sarah Elesbão <79275710+SarahElesbao@users.noreply.github.com>
2022-11-07 20:52:51 -0300

    Update Pessoa.java

--- codigos java/petshop/Pessoa.java
+++ codigos java/petshop/Pessoa.java
@@ -1,33 +1,37 @@
 package petshop;
 
 public class Pessoa {
-    String nome;
-    int telefone;
-    String cpf;
-    Endereco moradia;
-    Animal bicho;
+    private String nome;
+    private int telefone;
+    private Endereco moradia;
+    private Animal bicho;
 
     public void info() {
         System.out.printf("-_-_-_-_informações do dono do animal:-_-_-_-_\n\n");
         System.out.printf("Nome: %s\n", nome);
         System.out.printf("Número: %d\n", telefone);
-        System.out.printf("CPF: %s\n", cpf);
         System.out.printf("-_-_-_-_Endereco:-_-_-_-_\n\n");
-        System.out.printf("Rua: %s\n", moradia.rua);
-        System.out.printf("Número: %d\n", moradia.numero);
-        System.out.printf("Complemento: %s\n", moradia.complemento);
-        System.out.printf("Bairro: %s\n", moradia.bairro);
-        System.out.printf("Cidade: %s\n", moradia.cidade);
-        System.out.printf("CEP: %s\n", moradia.cep);
+        System.out.printf("Rua: %s\n", moradia.getRua());
+        System.out.printf("Número: %d\n", moradia.getNumero());
+        System.out.printf("Complemento: %s\n", moradia.getComplemento());
+        System.out.printf("Bairro: %s\n", moradia.getBairro());
+        System.out.printf("Cidade: %s\n", moradia.getCidade());
+        System.out.printf("CEP: %s\n", moradia.getCep());
         System.out.printf("-_-_-_-_Animal:-_-_-_-_\n\n");
-        System.out.printf("Nome: %s\n", bicho.nome);
-        System.out.printf("Raça: %s\n", bicho.raca);
-        System.out.printf("Porte: %s\n", bicho.porte);
-        System.out.printf("Bicho: %s\n", bicho.tipo);
-        System.out.printf("Peso: %f\n", bicho.peso);
-        // System.out.printf("Nascimento: %s\n", bicho.dtnascimento);
-        System.out.printf("Sexo: %c\n", bicho.sexo);
+        System.out.printf("Nome: %s\n", bicho.getNome());
+        System.out.printf("Raça: %s\n", bicho.getRaca());
+        System.out.printf("Porte: %s\n", bicho.getPorte());
+        System.out.printf("Bicho: %s\n", bicho.getTipo());
+        System.out.printf("Peso: %f\n", bicho.getPeso());
+        System.out.printf("Nascimento: %s\n", bicho.getDtnascimento());
+        System.out.printf("Sexo: %c\n", bicho.getSexo());
+    }
 
+    public Pessoa(String nome, int telefone, Endereco moradia, Animal bicho) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.moradia = moradia;
+        this.bicho = bicho;
     }
 
     public String nome() {
@@ -36,10 +40,6 @@ public class Pessoa {
 
     public int telefone() {
         return this.telefone;
-    }
-
-    public String cpf() {
-        return this.cpf;
     }
 
     public String getNome() {
@@ -56,14 +56,6 @@ public class Pessoa {
 
     public void setTelefone(int telefone) {
         this.telefone = telefone;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
     }
 
     public Endereco getMoradia() {
